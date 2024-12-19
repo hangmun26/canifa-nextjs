10cb29f13b4ce09c527d0d4b71889a9daf9e262a
hangmun <hangmun260896@gmail.com>
2024-12-19 17:25:45 +0700

    sua search

--- src/components/HeartIcon.js
+++ src/components/HeartIcon.js
@@ -1,6 +1,5 @@
 // HeartIcon.js
-import { useState } from "react";
-import SearchBar from "./SearchBar";
+import { useState } from 'react';
 
 function HeartIcon() {
   const [isClicked, setIsClicked] = useState(false);
@@ -13,9 +12,9 @@ function HeartIcon() {
     <i
       className="fa-regular fa-heart grid__column-3-icon"
       style={{
-        color: isClicked ? "red" : "black", // Đổi màu thành đỏ khi nhấn
-        transition: "color 0.3s", // Hiệu ứng chuyển màu mượt
-        cursor: "pointer", // Thay đổi con trỏ chuột khi hover
+        color: isClicked ? 'red' : 'black', // Đổi màu thành đỏ khi nhấn
+        transition: 'color 0.3s', // Hiệu ứng chuyển màu mượt
+        cursor: 'pointer', // Thay đổi con trỏ chuột khi hover
       }}
       onClick={handleIconClick} // Xử lý sự kiện nhấn
     ></i>
